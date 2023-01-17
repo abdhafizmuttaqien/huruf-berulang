32bf3deec46e12454b36581100014126f81e41c4
Abdul Hafiz Muttaqien <abdhafizmuttaqien@gmail.com>
2023-01-17 21:05:09 +0700

    first comit

---
+++ src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String input = "BCABA";
+        boolean found = false;
+        char character = 0;
+
+        for(int i=0; i<input.length(); i++){         
+            if(!found){
+                character = input.charAt(i);  
+                for(int j=i+1; j<input.length(); j++){
+                    if(character == input.charAt(j)){
+                        found = true;
+                        break;
+                    }
+                }
+            }
+            else{
+                break;
+            }
+        }
+
+        if(found == true){
+            System.out.println("Huruf berulang pertama adalah "+character);
+        }
+        else{
+            System.out.println("Tidak ada huruf berulang");
+        }
+    }
+}
